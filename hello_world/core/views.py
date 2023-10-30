2f17dab84e8c11925b330315b995578c81ba9653
SIIT3100 <112370307+SIIT3100@users.noreply.github.com>
2023-10-30 06:56:10 +0000

    Co-authored-by: 6322772094 <6322772094@users.noreply.github.com>

--- hello_world/core/views.py
+++ hello_world/core/views.py
@@ -5,3 +5,16 @@ def index(request):
         "title": "Django example",
     }
     return render(request, "index.html", context)
+
+def login(request):
+    context = {}
+    return render(request, "WebApp/login.html", context=context)
+
+def signup(request):
+   context = {}
+   return render(request, 'WebApp/reg.html', context=context)
+
+def home(request):
+   context = {}
+   return render(request, "WebApp/home.html", context=context)
+
--- hello_world/urls.py
+++ hello_world/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+# from . import views
 
 from hello_world.core import views as core_views
 
@@ -24,6 +25,9 @@ urlpatterns = [
     path("", core_views.index),
     path("admin/", admin.site.urls),
     path("__reload__/", include("django_browser_reload.urls")),
+    path("application/login", core_views.login),
+    path("application/register", core_views.signup),
+    path("application/home", core_views.home),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
