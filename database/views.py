0c1836b83c2063545ad6f3b31559ef9b3b62a405
Ayush Kwatra <113280814+6322774645@users.noreply.github.com>
2023-11-05 06:13:01 +0000

    reg send data to list view 2

--- database/views.py
+++ database/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.http import HttpResponse
+from .models import User
+import os
 
-# Create your views here.
+def signup(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        confirm_password = request.POST.get('confirm_password')
+
+        if password == confirm_password:
+            if User.objects.filter(username=username).exists():
+                messages.error(request, 'Username is already taken')
+                return redirect('application/register')  # Redirect to the signup page if username exists
+
+            if User.objects.filter(email=email).exists():
+                messages.error(request, 'Email is already registered')
+                return redirect('application/register')  # Redirect to the signup page if email exists
+
+            new_user = User(username=username, email=email, password=password)
+            new_user.save()
+
+            messages.success(request, 'You are registered and can now log in')
+            return redirect('application/login')  # Redirect to the login page
+        else:
+            messages.error(request, 'Passwords do not match')
+            return redirect('application/register')  # Redirect to the signup page
+
+    return render(request, 'WebApp/reg.html')
+
+
