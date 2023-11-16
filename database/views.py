509bb3cd57e95311fe76c20856d90228f6955a0b
KJ <6322773100@g.siit.tu.ac.th>
2023-11-16 19:17:17 +0700

    Done

--- database/views.py
+++ database/views.py
@@ -184,18 +184,29 @@ def database_item_add(request, uid):
             item.file.save("new_file.json", combined_json_file)
             fid = item.fID
 
-            return redirect('core_views_homeShowTest' , fid=fid)
+            return redirect('database_homeShowTest' , fid=fid)
     
     
     return redirect('database_item_upload', uid=uid)
 
 def database_statistic (request, fid):
-    dataset_objs = File.objects.filter(fUID=fid)
+    try:
+        item = File.objects.get(fID=fid)
+    except File.DoesNotExist:
+        return HttpResponse("ID Not found")
+
+    # Extract the content of the 'file' FieldFile as a string
+    try:
+        file_content = item.file.read().decode('utf-8')  # Assuming it's a text file
+    except AttributeError:
+        return HttpResponse("File content not found")
+    
+    json_data = json.loads(file_content)
     #if len(dataset_objs) <= 0:
         #return HttpResponse("ID Not found" )
     context_data = {
-        "filter_type":str(id),
-        "datasets":dataset_objs
+        "filter_type":str(fid),
+        "datasets":json_data
     }
     return render(request, 'WebApp/homeShowTest.html' , context= context_data)
             
@@ -210,8 +221,8 @@ def database_item_list_by_id (request, fuid):
     }
     return render(request, 'WebApp/view.html' , context= context_data)
 
-def database_item_delete(request, id):
-    dataset_objs = File.objects.filter(fID=id)
+def database_item_delete(request, fid):
+    dataset_objs = File.objects.filter(fID=fid)
     if len(dataset_objs) <= 0:
         return HttpResponse("ID Not found")
     Uid = dataset_objs.first().fUID.UID
@@ -219,16 +230,16 @@ def database_item_delete(request, id):
 
     dataset_objs = File.objects.filter(fUID__UID=Uid)
     context_data = {
-        "filter_type":str(id),
+        "filter_type":str(fid),
         "datasets":dataset_objs
     }
     #dataset_objs.delete()
     return render(request, 'WebApp/view.html' , context= context_data)
 
 
-def database_item_edit(request, id):
+def database_item_edit(request, fid):
     try:
-        item = File.objects.get(fID=id)
+        item = File.objects.get(fID=fid)
     except File.DoesNotExist:
         return HttpResponse("ID Not found")
 
@@ -239,56 +250,26 @@ def database_item_edit(request, id):
         return HttpResponse("File content not found")
     
     json_data = json.loads(file_content)
-
-    # Print the JSON data for debugging
-    print(json_data)
-
-    url = "https://twinword-twinword-bundle-v1.p.rapidapi.com/sentiment_analyze/"
-
-    headers = {
-        "X-RapidAPI-Key": "88584e470amsh820791e23792648p1e6451jsne53bd64fe92d",
-        "X-RapidAPI-Host": "twinword-twinword-bundle-v1.p.rapidapi.com"
-    }
-
-    responses = []
-    combined_data=[]
-
-    for sentence_data in json_data:
-        sentence = sentence_data.get("message", "")
-        querystring = {"text": sentence}
-        response = requests.get(url, headers=headers, params=querystring)
-        responses.append(response.json())
-
-        #print(response.json())
-
-        # Combine message and response into a dictionary
-        combined_entry = {
-            "message": sentence,
-            "sentiment_response": response.json()
-        }
-
-        combined_data.append(combined_entry)
-
-    #print(responses)
-    print(combined_data)
-    
-    # Serialize the combined data as a JSON string
-    combined_json = json.dumps(combined_data)
-
-    # Create a ContentFile from the serialized JSON data
-    combined_json_file = ContentFile(combined_json.encode('utf-8'))
-
-    # Update the 'file' field of the item with the new content
-    item.file.save("new_file.json", combined_json_file)
-
+    #if len(dataset_objs) <= 0:
+        #return HttpResponse("ID Not found" )
     context_data = {
-        'item_id': id,
-        'form_data': {
-            'file': json_data
-        }
+        "filter_type":str(fid),
+        "datasets":json_data
     }
+    return render(request, 'WebApp/editfile.html' , context= context_data)
 
-    return render(request, 'WebApp/editfile.html', context= context_data)
+# views.py
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+
+def submit_messages(request):
+    if request.method == 'POST':
+        messages_json = request.POST.get('messages')
+        messages = json.loads(messages_json)
+        
+        # Process the messages as needed (e.g., save to the database)
+
+        return JsonResponse({'status': 'success'})
 
 #api
 @csrf_exempt
--- hello_world/urls.py
+++ hello_world/urls.py
@@ -28,23 +28,23 @@ urlpatterns = [
     path("", core_views.index),
     path("admin/", admin.site.urls),
     path("__reload__/", include("django_browser_reload.urls")),
-    path("application/login", core_views.login),
+    path("application/login", core_views.login, name="core_views_login"),
     path("application/logins/fn.hidden", database_views.database_login,name="database_login"),
     path("application/register", core_views.signup, name="register"),
     path("application/register/fn.hidden", database_views.database_create_new_user,name="database_create_new_user"),
     path("application/home/<uid>", database_views.database_item_upload, name="database_item_upload"), # home/upload
     path("application/home.result", core_views.home2), # result
-    path("application/home.result/<fid>", database_views.database_statistic, name="core_views_homeShowTest"),
-    path("application/about", core_views.aboutus),
+    path("application/home.result/<fid>", database_views.database_statistic, name="database_homeShowTest"),
     path("application/files/add/<uid>", database_views.database_item_add,name="database_item_add"),
     path("application/files/<fuid>", database_views.database_item_list_by_id),
-    #path("application/files/s/<fuid>", # specific file
-    path("application/files/delete/<id>", database_views.database_item_delete),
-    path("application/files/edit/<id>", database_views.database_item_edit),
+    #path("application/files/s/<fuid>", # specific file)
+    path("application/files/delete/<fid>", database_views.database_item_delete),
+    path("application/files/edit/<fid>", database_views.database_item_edit, name="database_item_edit"),
+    #path("application/files/update", database_views.database_update, name="database_update"),
     path("api/delete/<id>", database_views.api_item_delete),
     path("api/process/<id>", database_views.api_item_process),
+    #path('submit_messages/', database_views.submit_messages, name='submit_messages'),
 ]
-
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
