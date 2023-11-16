8f7b7d64347c3b1b0bd185eff339f1e6ee57b3f3
TheerachoteTan <150259030+TheerachoteTan@users.noreply.github.com>
2023-11-16 07:09:34 +0000

    still error on editfile

--- database/Form.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-
-
-class UploadFileForm(forms.Form):
-    fName = forms.CharField(max_length=100)
-    file = forms.FileField(upload_to='file/')
-    fDateTime = forms.DateTimeField()
